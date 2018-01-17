01ae3d1eeb82745f2b23c93ab62c79317df2e9cf
Michael Pauly <micscopau@gmail.com>
2018-01-17 12:50:46 -0700

    CapturedPictureActivity Layout finished
    Next steps:
    Code for CapturedPictureActivity
    Retake picture button appears - back button resume preview (unless preview visible, then back to previous screen)
    OpenCV image processing on circles is displayed on captured image
    Circle count is displayed with option to increase/decrease count (+50 button?)

--- app/src/main/java/paulygon/helloopencv/CapturedPictureActivity.java
+++ app/src/main/java/paulygon/helloopencv/CapturedPictureActivity.java
@@ -2,6 +2,7 @@ package paulygon.helloopencv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CapturedPictureActivity extends AppCompatActivity {
 
@@ -10,4 +11,24 @@ public class CapturedPictureActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_captured_picture);
     }
+
+    public void fabAcceptClick(View view) {
+    }
+
+    public void fabCameraClick(View view) {
+    }
+
+    public void bttnMinusFiftyClick(View view) {
+    }
+
+    public void bttnMinusOneClick(View view) {
+    }
+
+    public void bttnPlusOneClick(View view) {
+    }
+
+    public void bttnPlusFiftyClick(View view) {
+    }
+
+
 }
