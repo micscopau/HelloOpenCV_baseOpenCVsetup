d7c5f5045a3f290f512ca56909758bd9ea9df719
Michael Pauly <micscopau@gmail.com>
2018-01-17 10:45:45 -0700

    Captured Image activity added - needs code and finishing layout touches
    How to handle/display back button for retake picture?
    +50 points button?
    
    Next steps:
    Retake picture button appears - back button resume preview (unless preview visible, then back to previous screen)
    OpenCV image processing on circles is displayed on captured image
    Circle count is displayed with option to increase/decrease count (+50 button?)

---
+++ app/src/main/java/paulygon/helloopencv/CapturedPictureActivity.java
@@ -0,0 +1,13 @@
+package paulygon.helloopencv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CapturedPictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_captured_picture);
+    }
+}
